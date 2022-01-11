8d11a29c07bae8b4da9a78658637ec1a17fe3532
antoniotex <acm.skt@gmail.com>
2022-01-11 00:34:09 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -5,11 +5,13 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AccountModel } from '@/domain/models/account-model'
 
 export class RemoteAuthentication {
   constructor (
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient) {}
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
     const httpResponse = await this.httpPostClient.post({
