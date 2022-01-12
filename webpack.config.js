0cb9a3e6c4e6d5923b808fd7994e1e1ec392085a
antoniotex <acm.skt@gmail.com>
2022-01-11 23:45:23 -0300

    chore: add config ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,18 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  }
   devServer: {
     // contentBase
     static : {
