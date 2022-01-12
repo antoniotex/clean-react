925418d092371cfa307037ae167f7c07ffccaee9
antoniotex <acm.skt@gmail.com>
2022-01-11 23:41:42 -0300

    chore: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    // contentBase
+    static : {
+      directory : path.join(__dirname, "public/")
+    },
+    // hotOnly
+    hot: "only",
+    // Deixa que coloque qualquer rota, que o react consegue resolver
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
