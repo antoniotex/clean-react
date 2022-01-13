b2248852c826e9e0cff394dadda6ec4d8f91844c
antoniotex <antoniotx.dev@gmail.com>
2022-01-13 00:01:15 -0300

    refactor: lint

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,6 +1,6 @@
-import { Login } from "@/presentation/pages";
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Login } from '@/presentation/pages'
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const Router: React.FC = () => {
   return (
