35508cb02ea5bf82daa9e81ff138fd2b6a478c16
antoniotex <acm.skt@gmail.com>
2022-02-02 00:07:04 -0300

    test: ensure Login present error and hide spinner on authentication fail

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
 import Login from './login'
 import { AuthenticationSpy, ValidationSpy } from '@/presentation/test'
 import faker from 'faker'
+import { InvalidCredencialsError } from '@/domain/errors'
 
 // Garantir que componentes Spinner e Erro não sejam renderizados no inicio
 // Garantir que Validation é chamada com email correto
@@ -130,5 +131,18 @@ describe('Login Component', () => {
     populateEmailField(sut)
     fireEvent.submit(sut.getByTestId('form'))
     expect(authenticationSpy.callsCount).toBe(0)
+  })
+
+  test('Should present error if Authentication fails', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const error = new InvalidCredencialsError()
+    jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error))
+    simulateValidSubmit(sut)
+    const errorWrap = sut.getByTestId('error-wrap')
+    // Esse waitFor é utilizado para operações assíncronas e espera o componente passado renderizar
+    await waitFor(() => errorWrap)
+    const mainError = sut.getByTestId('main-error')
+    expect(mainError.textContent).toBe(error.message)
+    expect(errorWrap.childElementCount).toBe(1)
   })
 })
