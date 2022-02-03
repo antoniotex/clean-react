f91f163832898798f95c5622a8be8f42fe95d01c
antoniotex <acm.skt@gmail.com>
2022-02-02 21:16:29 -0300

    feat: ensure RequiredFieldValidation returns errors if field is empty

---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FieldValidation } from '@/validation/protocols'
+import { RequiredFieldError } from '@/validation/errors'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly field: string) {
+
+  }
+
+  validate (value: string): Error {
+    return new RequiredFieldError()
+  }
+}
