83576550ca0f366a0b1180b5856de609cb0884cd
antoniotex <acm.skt@gmail.com>
2022-02-02 23:18:44 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Validation } from '@/presentation/protocols/validation'
+import { FieldValidation } from '@/validation/protocols'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validators: FieldValidation[]) {}
+  validate (fieldName: string, fieldValue: string): string {
+    const validators = this.validators.filter(v => v.field === fieldName)
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue)
+      if (error) {
+        return error.message
+      }
+    }
+    return null
+  }
+}
