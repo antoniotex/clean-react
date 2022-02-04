1311d97d18ab0ca76e8e6c20023274ba1b833a7d
antoniotex <acm.skt@gmail.com>
2022-02-03 23:33:00 -0300

    refactor: move factories to their own files

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,21 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
-import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios-http-client'
 import { Login } from '@/presentation/pages'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
-import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
 import React from 'react'
+import { makeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory'
+import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const url = 'http://fordevs.herokuapp.com/api/login'
-  const axiosHttpClient = new AxiosHttpClient()
-  const remoteAuthentication = new RemoteAuthentication(url, axiosHttpClient)
-  const validationComposite = new ValidationComposite([
-    ...ValidationBuilder.field('email').required().email().build(),
-    ...ValidationBuilder.field('password').required().min(5).build()
-  ])
-
   return (
-    <Login validation={validationComposite} authentication={remoteAuthentication} />
+    <Login validation={makeLoginValidation()} authentication={makeRemoteAuthentication()} />
   )
 }
